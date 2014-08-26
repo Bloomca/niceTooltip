34cd621f6af35b72f0b374a070a85c7de209ec7f
Bloomca <seva.zaikov@gmail.com>
2014-08-27 01:12:35 +0400

    landing

--- js/nicetooltip.js
+++ js/nicetooltip.js
@@ -2,22 +2,33 @@
     var ctr = 0;
     $.fn.niceTooltip = function (options) {
         var transition = options.time || 300;
+        if (options.position) {
+            var position = {
+                left: (options.position.left === "left" || options.position.left === "right" || options.position.left === "center") ? options.position.left : "right",
+                top: (options.position.top === "top" || options.position.top === "bottom" || options.position.top === "center") ? options.position.top : "top"
+            };
+        } else position = {left: "right", top: "top"};
+
         // we have only 1 instance of tooltip
         var $b = $('body'),
             $tp = $b.find('>div#nice-tooltip-instance');
         if (!$tp.length) $tp = $('<div id="nice-tooltip-instance"></div>').appendTo('body').css($.fn.niceTooltip.defaults.css);
 
-        // TODO wrap with span
-//        .css({
-//            borderBottom: "1px dotted black",
-//            cursor: "help"
-//        })
+        if (this.prop('tagName')==="SPAN" && !options.disableBorder) this.css({
+            borderBottom: "1px dotted black",
+            cursor: "help"
+        });
+
+        var touch = 'ontouchstart' in document.documentElement,
+            ios = /(iPad|iPhone|iPod)/g.test( navigator.userAgent),
+            android = navigator.userAgent.toLowerCase().indexOf("android") > -1;
 
         this.data({
-                'nice-tooltip-content': options.HTML,
-                'nice-tooltip-id': ctr++
-            })
-            .on('mouseenter', function (e) {
+            'nice-tooltip-content': options.HTML,
+            'nice-tooltip-id': ctr++
+        });
+        if (!touch && !ios && !android) {
+            this.on('mouseenter', function (e) {
                 var $el = $(this),
                     tpId = $tp.data('id'),
                     thId = $el.data('nice-tooltip-id');
@@ -26,34 +37,36 @@
                     opacity:1
                 },transition);
             })
-            .on('mouseleave', function (e) {
-                $tp.stop().animate({opacity:0},transition);
-            });
+                .on('mouseleave', function (e) {
+                    $tp.stop().animate({opacity:0},transition);
+                });
 
-        if ($.fn.niceTooltip.defaults.move) {
-            var shift = +new Date;
-            this.on('mousemove', function (e) {
-                var newShift = +new Date;
-                if ((newShift - shift) < 15 ) return;
-                else {
-                    shift = newShift;
-                    fixPageXY(e);
-                    var coords = {
-                            left: e.pageX,
-                            top: e.pageY
-                        },
-                        w = $tp.width(),
-                        h = $tp.height(),
-                        aw = document.documentElement.clientWidth,
-                        ah = document.documentElement.clientHeight,
-                        left = (aw - coords.left - w - 20 > 0 ) ? (coords.left + 20) : (coords.left - w - 20),
-                        top = (ah - coords.top + h + 20 > 0 ) ? (coords.top - 20) : (coords.top + h + 20);
-                    $tp.css({
-                        left:left,
-                        top: top
-                    });
-                }
-            });
+            if ($.fn.niceTooltip.defaults.move) {
+                var shift = +new Date;
+                this.on('mousemove', function (e) {
+                    var newShift = +new Date;
+                    if ((newShift - shift) < 15 ) return;
+                    else {
+                        shift = newShift;
+                        fixPageXY(e);
+                        var coords = {
+                                left: e.pageX,
+                                top: e.pageY
+                            },
+                            w = $tp.width(),
+                            h = $tp.height(),
+                            aw = document.documentElement.clientWidth,
+                            ah = document.documentElement.clientHeight,
+                            obj = setPosition(aw, w, ah, h, coords);
+                        $tp.css({
+                            left:obj.left,
+                            top: obj.top
+                        });
+                    }
+                });
+            }
+        } else {
+
         }
 
         // compatible with old IE
@@ -68,6 +81,29 @@
                 e.pageY = e.clientY + (html.scrollTop || body && body.scrollTop || 0);
                 e.pageY -= html.clientTop || 0;
             }
+        }
+
+        function setPosition (aw, w, ah, h, coords) {
+            var obj = {},
+                left = {
+                    right: coords.left + 15,
+                    left: coords.left - w - 50,
+                    leftCheck: aw - coords.left - w - 20,
+                    rightCheck: coords.left - w - 20
+                },
+                top = {
+                    top: coords.top - h - 30,
+                    bottom: coords.top + 20,
+                    topCheck: ah - coords.top + h + 20,
+                    bottomCheck: coords.top - h + 20
+                };
+
+            if (position.left == "right") obj.left = (left.leftCheck > 0) ? left.right : left.left;
+            else if (position.left == "left") obj.left = (left.rightCheck <= 0) ? left.right : left.left;
+            if (position.top == "top") obj.top = (top.topCheck > 0) ? top.top : top.bottom;
+            else obj.top = (top.bottomCheck <= 0) ? top.top : top.bottom;
+
+            return obj;
         }
 
     };
